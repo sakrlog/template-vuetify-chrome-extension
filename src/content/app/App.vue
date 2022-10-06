644ade60d2e61781b2324301a9580aa91b68f9a0
sakrlog <sakr.log@gmail.com>
2022-10-06 11:34:17 +0300

    open and close functionality done

--- src/content/app/App.vue
+++ src/content/app/App.vue
@@ -1,42 +1,47 @@
 <template>
   <v-app>
     <v-card flat height="600px" tile>
-      <v-toolbar color="light-blue accent-4"
-  elevation="4">
+      <v-toolbar color="light-blue accent-4" elevation="4">
         <v-toolbar-title>Newsela helper Tool</v-toolbar-title>
 
         <v-spacer></v-spacer>
 
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
+        <v-btn icon @click="changeIframeSize()">
+          <v-icon>{{toolOpen?"mdi-lock":"mdi-lock-open"}}</v-icon>
         </v-btn>
 
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
         <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Tab 1</v-tab>
-          <v-tab>Tab 2</v-tab>
-          <v-tab>Tab 3</v-tab>
-        </v-tabs>
-      </template>
+          <v-tabs align-with-title>
+            <v-tab>Tab 1</v-tab>
+            <v-tab>Tab 2</v-tab>
+            <v-tab>Tab 3</v-tab>
+          </v-tabs>
+        </template>
       </v-toolbar>
     </v-card>
-</v-app>
+  </v-app>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      toolOpen: false,
+    };
   },
   name: "App",
   methods: {
+    changeIframeSize: function () {
+      // select the iframe and change its height to the opposite of what it is
+      let msg = {"function":"change-newselas-iframe-size"}
+      if(this.toolOpen){
+        msg["size"] = "50px"
+      }else{
+        msg["size"] = "600px"
+      }
+      this.toolOpen = !this.toolOpen
+      window.parent.postMessage(msg, "*")
+    },
     goBack: function () {
       this.$router.go(-1);
     },
--- src/content/content.js
+++ src/content/content.js
@@ -1,6 +1,6 @@
 if (!document.getElementById("newsela-control-bar")) {
   var attr =
-    " position: fixed;left: 0;bottom:0;width: 100%;height: 600px;text-align: center;background: black;border: 1px solid black;;z-index: 999999;";
+    " position: fixed;left: 0;bottom:0;width: 100%;height: 50px;text-align: center;background: black;border: 1px solid black;;z-index: 999999;";
   var source = chrome.runtime.getURL("content/main-newsela.html");
   var iframe = getOrCreateEl(
     "newsela-control-bar",
@@ -29,3 +29,14 @@ function getOrCreateEl(elId, elTag, parentEl, attributes, source) {
   }
   return el;
 }
+
+window.addEventListener("message", (event) => {
+  console.log("new message:", event.data)
+  if(event.data.function !== "change-newselas-iframe-size")
+  return
+  console.log("enetered the conetnet script from the other hand", event)
+  var iframe = document.getElementById("newsela-control-bar")
+  iframe.style.height = event.data.size
+
+  // …
+}, false);
