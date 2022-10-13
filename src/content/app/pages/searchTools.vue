5aa66703c4b6a3466a6880a84b0b4c60485153be
sakrlog <sakr.log@gmail.com>
2022-10-13 12:44:51 +0300

    search tools is done with the reccursinve alg and the treeview

--- src/content/app/pages/searchTools.vue
+++ src/content/app/pages/searchTools.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="black_texts" fluid>
     <v-row>
-      <v-col md="6">
+      <v-col md="5">
         <v-row>
           <v-col md="6">
             <v-text-field
@@ -41,9 +41,7 @@
                 v-for="(item, index) in items_to_display"
                 :key="item.url"
               >
-                <v-list-item
-                  @click="handleDetailedView(item.rights ? item.rights : null)"
-                >
+                <v-list-item @click="handleDetailedView(item)">
                   <template v-slot:default="{ active }">
                     <v-list-item-content>
                       <v-list-item-title
@@ -53,8 +51,8 @@
                       <v-list-item-subtitle>{{
                         "https://www.newsela.com" + item.url
                       }}</v-list-item-subtitle>
-                      
-                      {{item.description}}
+
+                      {{ item.description }}
                     </v-list-item-content>
                   </template>
                 </v-list-item>
@@ -68,7 +66,67 @@
         </v-card>
       </v-col>
       <v-divider vertical></v-divider>
-      <v-col md="6"> </v-col>
+      <v-col md="7">
+        <v-card height="620px" class="overflow-y-auto">
+          <v-row>
+            <v-col>
+              <v-treeview
+                v-model="treeSelection"
+                :items="articleComponents"
+                selectable
+                return-object
+              ></v-treeview>
+            </v-col>
+            <v-divider vertical></v-divider>
+            <v-col class="pa-6" cols="8">
+              <template v-if="!treeSelection.length">
+                No nodes selected.
+              </template>
+              <template v-else>
+                <v-list two-line v-if="treeSelection !== null">
+                  <v-list-item-group active-class="blue--text">
+                    <template
+                      v-for="(item, index) in treeSelection"
+                      :key="item.id"
+                    >
+                      <v-list-item>
+                        <template v-slot:default="{ active }">
+                          <v-list-item-content>
+                            <v-list-item-title
+                              v-text="item.name"
+                            ></v-list-item-title>
+
+                            <!-- <a
+                              v-if="
+                                /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/.test(
+                                  item.value
+                                ) == true
+                              "
+                              :href="item.name"
+                              target="_blank"
+                              >External URL
+                              <v-icon :href="item.name" target="_blank"
+                                >open-in-new</v-icon
+                              ></a
+                            > -->
+                            <!-- <span v-else>{{item.value}}</span> -->
+
+                            {{item.value}}
+                          </v-list-item-content>
+                        </template>
+                      </v-list-item>
+                      <v-divider
+                        v-if="index < treeSelection.length - 1"
+                        :key="index"
+                      ></v-divider>
+                    </template>
+                  </v-list-item-group>
+                </v-list>
+              </template>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -80,6 +138,8 @@ export default {
   name: "SearchTools",
   data() {
     return {
+      treeSelection: [],
+      articleComponents: [],
       parentWindowDetails: null,
       options: { q: "history" },
       newselaChipColor: true,
@@ -87,18 +147,6 @@ export default {
       alertWindow: null,
       items: null,
       items_to_display: null,
-      items_filters: [],
-      selected_filters: [],
-      specific_table_headers: [
-        {
-          text: "Element",
-          align: "start",
-          sortable: false,
-          value: "element",
-        },
-        { text: "Value", value: "value" },
-      ],
-      specific_table_infos: null,
     };
   },
   async created() {
@@ -123,7 +171,58 @@ export default {
     );
   },
   methods: {
-    handleDetailedView() {},
+    handleDetailedView(selected) {
+      let currentId = 0;
+      function createTreeView(data) {
+        let tree = [];
+        if (!data || data.length < 1 || data === null) {
+          console.log("--- DATA IS EMPTY ---");
+          return tree;
+        } else if (typeof data === "object" && !Array.isArray(data)) {
+          //This is an object {} for sure
+          const keys = Object.keys(data);
+          for (const key of keys) {
+            // k:key and v:data[key]
+            if (
+              typeof data[key] === "string" ||
+              typeof data[key] === "number"
+            ) {
+              const obj = { id: currentId++, name: key, value: data[key] };
+              tree.push(obj);
+            } else {
+              //could be an object or an array I don't know - send it back in reccursion
+              const obj = {
+                id: currentId++,
+                name: key,
+                children: createTreeView(data[key]),
+              };
+              tree.push(obj);
+            }
+          }
+        } else if (Array.isArray(data)) {
+          // The data is 100% an array, but I still don't know its content
+          for (const element of data) {
+            if (typeof element === "string" || typeof element === "number") {
+              const obj = { id: currentId++, name: element, value: element };
+              tree.push(obj);
+            } else {
+              //could be an object or an array I don't know - send it back in reccursion
+              const number = data.indexOf(element) + 1;
+              const obj = {
+                id: currentId++,
+                name: number,
+                children: createTreeView(element),
+              };
+              tree.push(obj);
+            }
+          }
+        }
+        return tree;
+      }
+      this.articleComponents = [];
+      this.treeSelection = [];
+      this.articleComponents = createTreeView(selected);
+    },
     async chipClick(chip) {
       if (chip == "alexandria") {
         // If alexandria filter is clicked
@@ -212,7 +311,7 @@ export default {
       });
 
       return site_info;
-    },
+    }
   },
 };
 </script>
